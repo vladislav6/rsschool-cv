bb708a78700e95c2757c038c388fa4943a8a6b26
Vladislav <vlad.kvan96@gmail.com>
2026-03-20 00:16:01 +0200

    feat: add adaptive and mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 import '../scss/style.scss';
 
 hljs.highlightElement(document.querySelector('pre code'));
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.menu');
+const menuList = document.querySelector('.menu ul');
+const body = document.body;
+
+mobileMenu.addEventListener('click', () => {
+  body.classList.add('scroll-hiden');
+  menu.classList.add('mobile');
+  if (menu.classList.contains('mobile')) {
+    mobileMenu.classList.toggle('close');
+  }
+
+  if (!mobileMenu.classList.contains('close')) {
+    body.classList.remove('scroll-hiden');
+    menu.classList.remove('mobile');
+  }
+});
+
+menuList.addEventListener('click', () => {
+  body.classList.remove('scroll-hiden');
+  menu.classList.remove('mobile');
+  mobileMenu.classList.remove('close');
+});
